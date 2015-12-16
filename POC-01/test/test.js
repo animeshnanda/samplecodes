35cad6736b1e47302614aff677a9e96a68246b9b
animeshnanda <animeshnanda@gmail.com>
2015-12-16 23:14:57 +0530

    Chai Mocha Initialization

---
+++ POC-01/test/test.js
@@ -0,0 +1,14 @@
+	
+	var expect = require('chai').expect;
+	
+	describe('A basic test', function(){
+		it('should pass when everything is okay', function(){
+			expect(true).to.be.true;
+		});
+	});
+	
+	describe('A basic test 2', function(){
+		it('should pass when everything is okay and awesome', function(){
+			expect(true).to.be.true;
+		});
+	});
